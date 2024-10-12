a54a7cbb2e1f0e048f9630803defb5d3c1f86393
whitewizardd <leumasre@gmail.com>
2024-10-12 12:00:57 +0100

    dao contract underway

--- src/dao/Dao.sol
+++ src/dao/Dao.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+import {DaoObjects } from "./libraries/DaoObjects.sol";
+
 contract Dao {
 
-
+    uint256 public quorum;
+    uint256 public votingDuration;
+    uint256 public proposalCount;
+    mapping (address => uint256) public membershipBalance;
+    mapping (uint256 => mapping (address => bool)) private hasVoted;
+    mapping (address => bool) private isBlacklisted;
+    mapping (uint => DaoObjects.Proposal) public allProposals;
+    
     receive() external payable {}
 }
---
+++ src/dao/libraries/DaoObjects.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+library DaoObjects {
+    struct Proposal {
+        uint256 id;
+        bytes description;
+        address proposalCreator;
+        bool isExecuted;
+        uint128 createdOn;
+        uint128 deadline;
+    }
+}
